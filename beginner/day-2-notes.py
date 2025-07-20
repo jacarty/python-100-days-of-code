f158c54cd0d0b10e2487a6b27552638c2bd191f5
James Carty <jamescarty@hotmail.co.uk>
2025-07-20 16:03:52 +0100

    Add initial day2 progress

---
+++ beginner/day-2-notes.py
@@ -0,0 +1,42 @@
+"""
+Day Two Content - Data Types, Numbers, Operations, Type Conversations, f-Strings
+
+Looks like we'll be making a tip calculator for the Task
+"""
+
+# Reminder - getting length of a string
+print((len("hello")))
+
+"""
+This would error as it's not a string
+print((len(12345)))
+
+Data Types - String, Interger, Float, Boolean
+"""
+
+# Subscripting
+# We always start counting at zero 
+# This returns the character in specified position, e.g. (H) or (o)
+print("Hello"[0])
+print("Hello"[4])
+
+# Negative numbers start at the end and returns (o) or (e)
+print("Hello"[-1])
+print("Hello"[-4])
+
+""" 
+Integer is a whole number
+"""
+
+# This will do addition (579)
+print(123 + 456)
+
+# Large Integerr - underscores are like commas in large numbers and just for readability
+print(123_456_789)
+
+# Floats = Floating Point Number
+print(3.14159)
+
+# Boolean - true or false
+print(True)
+print(False)
