d9edc1f5823e91b3be08aaf6e6691226784ce250
jcarty <jamescarty@hotmail.co.uk>
2025-07-25 09:58:10 +0100

    Complete Day 7

---
+++ beginner/day_7_hangman_words.py
@@ -0,0 +1,61 @@
+word_list = [
+    # Animals
+    "aardvark",
+    "baboon",
+    "camel",
+    "dolphin",
+    "elephant",
+    "flamingo",
+    "giraffe",
+    "hamster",
+    "iguana",
+    "jaguar",
+    
+    # Food & Drinks
+    "avocado",
+    "banana",
+    "chocolate",
+    "donut",
+    "espresso",
+    "falafel",
+    "granola",
+    "hamburger",
+    "ice cream",
+    "jalapeno",
+    
+    # Common Objects
+    "airplane",
+    "bicycle",
+    "computer",
+    "dictionary",
+    "envelope",
+    "furniture",
+    "guitar",
+    "headphones",
+    "island",
+    "jacket",
+    
+    # Nature & Weather
+    "aurora",
+    "blizzard",
+    "canyon",
+    "desert",
+    "earthquake",
+    "forest",
+    "galaxy",
+    "hurricane",
+    "iceberg",
+    "jungle",
+    
+    # Misc Words
+    "knight",
+    "library",
+    "mountain",
+    "notebook",
+    "oxygen",
+    "pyramid",
+    "question",
+    "rainbow",
+    "sandwich",
+    "treasure"
+]
---
+++ beginner/day_7_task.py
@@ -0,0 +1,79 @@
+import random
+import day_7_hangman_art
+import day_7_hangman_words
+
+print(day_7_hangman_art.logo)
+
+# old word list for building
+# word_list = ["aardvark", "baboon", "camel", "dog", "elephant"]
+
+# select work from word list
+# chosen_word = random.choice(word_list)
+# use random to pick a random word from the list that is imported from hangman_words
+chosen_word = random.choice(day_7_hangman_words.word_list)
+# print(chosen_word)
+
+## placeholder = "_" * len(chosen_word) <---- This is the most pythonic way to do the loop below
+
+# migrate the string to a dictionary and put in _ for each character
+word_status = []
+for each_character in chosen_word:
+    word_status.append("_")
+
+# need a variable to use in while loop
+game_over = False
+# track guessed letters
+guessed_letters = []
+# add maximum number of lives to track
+lives = 6
+
+# loop while game over is False
+while not game_over:
+    # get input and set to lower case
+    guess = input("Please choose a letter?\n").lower()
+
+    # checks if they already guessed that letter
+    if guess in guessed_letters:
+        # return current game_status to the user
+        print(day_7_hangman_art.stages[lives])
+        print(f"You already guessed {guess} - try again :)")
+        print(''.join(word_status))
+        continue
+
+    # add guess to guessed letters for tracking
+    guessed_letters.append(guess)
+    # print(guessed_letters)
+
+    # add higher if statement to check it letter in chosed word
+    if guess in chosen_word:
+        # use enumerate to returns index, and character
+        # get the two variables for the loop
+        for index, character in enumerate(chosen_word):
+            if character == guess:
+                word_status[index] = guess
+        # return current game_status to the user
+        # lookup the ascii stage in the list based on current lives
+        print(day_7_hangman_art.stages[lives])
+        print(f"Correct, {guess} is in the word.")
+        print(''.join(word_status))
+    # reduce lives if not in word
+    else:
+        lives -= 1
+        # return current game_status to the user
+        print(day_7_hangman_art.stages[lives])
+        print(f"You guessed incorrectly, {guess} is not in the word.")
+        print(''.join(word_status))
+    
+    # end game if lives run out
+    if lives == 0:
+        print(day_7_hangman_art.stages[lives])
+        print(''.join(word_status))
+        print("GAME OVER")
+        print(f"The word was: {chosen_word}")
+        game_over = True
+    # loop until there are no _ in word_status 
+    elif "_" not in word_status:
+        print(day_7_hangman_art.stages[lives])
+        print(''.join(word_status))
+        print("Congratulations - you win!")
+        game_over = True
