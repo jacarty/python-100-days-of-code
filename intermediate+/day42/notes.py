7ea64ea25cbb0a1e6563d09be8c9b1a22775e12f
James Carty <jamescarty@hotmail.co.uk>
2025-09-10 11:47:50 +0100

    Day 42 Complete

---
+++ intermediate+/day42/notes.py
@@ -0,0 +1,103 @@
+"""
+The HTML Boilderplate
+
+Understanding HTML Structure
+"""
+
+###########################
+# Boilderplate
+###########################
+
+# <!-- Set to HTML5 -->
+# <!DOCTYPE html>
+# <!-- Specify language; useful for screen readers -->
+# <html lang="en">
+    
+#     <!-- Page data for browser -->
+#      <head>
+#         <!-- Specify page is using UTF-8 characters-->
+#         <meta charset="UTF-8">
+#         <!-- Specify Page Title -->
+#         <title>Page Title</title>
+#     </head>
+
+#     <!-- Page content -->
+#     <body>
+#         <h1>Hello World!</h1>
+#     </body>
+# </html>
+
+###########################
+# Lists
+###########################
+
+# <h2>Unordered List...</h2>
+# <ul>
+#     <li>milk</li>
+#     <li>bread</li>
+#     <li>cheese</li>
+#     <li>bacon</li>
+# </ul>
+# <h2>Ordered List...</h2>
+# <ol>
+#     <li>milk</li>
+#     <li>bread</li>
+#     <li>cheese</li>
+#     <li>bacon</li>
+# </ol>
+
+###########################
+# Nested Lists
+###########################
+
+# <ul>
+#     <li>milk</li>
+#     <li>bread</li>
+#     <li>brown sauce</li>
+#     <li>bacon</li>
+#     <li>cheeses:
+#         <ul>
+#             <li>cheddar</li>
+#             <li>jarlsberg</li>
+#             <li>port salut</li>
+#         </ul>
+#     </li>
+# </ul>
+
+###########################
+# Anchor Tags
+###########################
+
+# URL
+# <a href="https://www.google.com/">Google Link</a>
+
+# Draggable
+# <a draggable="true" href="https://www.google.com/">Google Link</a>
+
+# Image
+# <!-- Random Image Website-->
+# <img src="https://picsum.photos/400" alt="Random photo via picsum.photos"/><br>
+
+
+###########################
+# Birthday Site Example
+###########################
+
+# <!-- This is one possible solution -->
+# <h1>It's My Birthday!</h1>
+# <h2>On the 12th May</h2>
+
+# <img src="https://raw.githubusercontent.com/appbrewery/webdev/main/birthday-cake3.4.jpeg"
+#   alt="purple birthday cake with candles" />
+
+# <h3>What to bring:</h3>
+# <ul>
+#   <li>Baloons (I love baloons)</li>
+#   <li>Cake (I'm really good at eating)</li>
+#   <li>An appetite (There will be lots of food)</li>
+# </ul>
+
+# <h3>This is where you need to go:</h3>
+# <a
+#   href="https://www.google.com/maps/@35.7040744,139.5577317,3a,75y,289.6h,87.01t,0.72r/data=!3m6!1e1!3m4!1sgT28ssf0BB2LxZ63JNcL1w!2e0!7i13312!8i6656">Google
+#   map link</a>
