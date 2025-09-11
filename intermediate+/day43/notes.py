645efab1a0b33362d1d22c508290514d8f327688
James Carty <jamescarty@hotmail.co.uk>
2025-09-11 15:22:15 +0100

    Day 43 Complete

---
+++ intermediate+/day43/notes.py
@@ -0,0 +1,45 @@
+"""
+Cascading Style Sheets
+CSS - Why Do We Need It?
+
+https://appbrewery.github.io/just-add-css/
+
+You can add in three ways:
+
+-- Inline 
+-- Was used for a particular Element
+<tag style="css"/>
+
+-- Internal
+-- Was used for an individual Page
+<style>css</style>
+
+-- External
+-- Used for an entire site
+<link 
+    rel="stylesheet" 
+    href="./style.css"
+/>
+
+"""
+# Should be Unique within HTML
+# #ID-Selector
+# #main{
+# color: red;
+# }
+
+# Many elements
+# .Class-Selector
+# .red-text{
+# color: red;
+# }
+
+# Attiribute selector
+# p[draggable]{
+# color: red;
+# }
+
+# Universal selector
+# *{
+# color: red;
+# }
