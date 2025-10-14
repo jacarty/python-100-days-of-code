447ae6007b7c38e7d81e6a1a428b08fb2a808efd
James Carty <jamescarty@hotmail.co.uk>
2025-10-14 14:55:54 +0100

    Day 73 Complete

---
+++ advanced/day73/notes.py
@@ -0,0 +1,21 @@
+"""
+Day 73 Goals: what you will make by the end of the day
+
+Analyse the Popularity of Different Programming Languages over Time
+
+The oldest programming language still in use today is FORTRAN, which was developed in 1957. Since then many other programming languages have been developed. But which programming language is the most popular? Which programming language is the Kim Kardashian of programming languages; the one people just can't stop talking about? 
+
+StackOverflow will help us answer this burning question. Each post on Stack OverFlow comes with a Tag. And this Tag can be the name of a programming language.
+
+To figure out which language is the most popular, all we need to do is count the number of posts on Stack Overflow that are tagged with each language. The language with the most posts wins!
+
+Today you will learn:
+
+    How to visualise your data and create charts with Matplotlib
+
+    How to pivot, group and manipulate your data with Pandas to get it into the format you want
+
+    How to work with timestamps and time-series data
+
+    How to style and customise a line chart to your liking
+"""
