fefcfc80ba5b61fa744901084837438f703ad555
James Carty <jamescarty@hotmail.co.uk>
2025-10-14 14:55:42 +0100

    Day 72 Complete

---
+++ advanced/day72/notes.py
@@ -0,0 +1,29 @@
+"""
+Day 72 Goals: what you will make by the end of the day
+
+Learn Data Exploration with Pandas by Analysing the Post-University Salaries of Graduates by Major
+
+College degrees are very expensive. But, do they pay you back? Choosing Philosophy or International Relations as a major may have worried your parents, but does the data back up their fears? PayScale Inc. did a year-long survey of 1.2 million Americans with only a bachelor's degree. We'll be digging into this data and use Pandas to answer these questions:
+
+    Which degrees have the highest starting salaries? 
+
+    Which majors have the lowest earnings after college?
+
+    Which degrees have the highest earning potential?
+
+    What are the lowest risk college majors from an earnings standpoint?
+
+    Do business, STEM (Science, Technology, Engineering, Mathematics) or HASS (Humanities, Arts, Social Science) degrees earn more on average? 
+
+Today you'll learn
+
+    How to explore a Pandas DataFrame
+
+    How to detect NaN (not a number) values and clean your data
+
+    How to select particular columns, rows, and individual cells
+
+    How to sort your data
+
+    How to group data by category
+"""
--- advanced_notes.py
+++ advanced_notes.py
@@ -2631,3 +2631,31 @@ class LoginForm(FlaskForm):
 class CommentForm(FlaskForm):
     comment_text = CKEditorField("Comment", validators=[DataRequired()])
     submit = SubmitField("Submit Comment")
+
+##############################################################################
+# DAY 72 - Pandas Data Manipulation
+##############################################################################
+
+# Use .head(), .tail(), .shape and .columns to explore your DataFrame and find out the number of rows and columns as well as the column names.
+
+# Look for NaN (not a number) values with .findna() and consider using .dropna() to clean up your DataFrame.
+
+# You can access entire columns of a DataFrame using the square bracket notation: df['column name'] or df[['column name 1', 'column name 2', 'column name 3']]
+
+# You can access individual cells in a DataFrame by chaining square brackets df['column name'][index] or using df['column name'].loc[index]
+
+# The largest and smallest values, as well as their positions, can be found with methods like .max(), .min(), .idxmax() and .idxmin()
+
+# You can sort the DataFrame with .sort_values() and add new columns with .insert()
+
+# To create an Excel Style Pivot Table by grouping entries that belong to a particular category use the .groupby() method
+
+##############################################################################
+# DAY 73 - Data Visualisation with Metaplotlib
+#
+# How to visualise your data and create charts with Matplotlib
+# How to pivot, group and manipulate your data with Pandas to get it into the format you want
+# How to work with timestamps and time-series data
+# How to style and customise a line chart to your liking
+##############################################################################
+
