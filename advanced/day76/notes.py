14c92bd4d9cd30f7f1c25d6b2ad9c73f671890d8
James Carty <jamescarty@hotmail.co.uk>
2025-10-17 17:33:14 +0100

    Day 76 Complete

---
+++ advanced/day76/notes.py
@@ -0,0 +1,44 @@
+"""
+In this module, we will compare thousands of apps in the Google Play Store so that we can gain insight into:
+
+    How competitive different app categories (e.g., Games, Lifestyle, Weather) are
+
+    Which app category offers compelling opportunities based on its popularity
+
+    How many downloads you would give up by making your app paid vs. free
+
+    How much you can reasonably charge for a paid app
+
+    Which paid apps have had the highest revenue
+
+    How many paid apps will recoup their development costs based on their sales revenue
+
+
+Today you'll learn:
+
+    How to quickly remove duplicates
+
+    How to remove unwanted symbols and convert data into a numeric format
+
+    How to wrangle columns containing nested data with Pandas
+
+    How to create compelling data visualisations with the plotly library
+
+    Create vertical, horizontal and grouped bar charts
+
+    Create pie and donut charts for categorical data
+
+    Use colour scales to make beautiful scatter plots
+"""
+
+"""
+In this lesson we looked at how to:
+
+    Pull a random sample from a DataFrame using .sample()
+
+    How to find duplicate entries with .duplicated() and .drop_duplicates()
+
+    How to convert string and object data types into numbers with .to_numeric()
+
+    How to use plotly to generate beautiful pie, donut, and bar charts as well as box and scatter plots 
+"""
