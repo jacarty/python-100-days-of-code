4dd60c25b9430979beb24b9a98a96c3c0fca5c56
James Carty <jamescarty@hotmail.co.uk>
2025-10-19 15:16:36 +0100

    Day 78 Complete

---
+++ advanced/day78/notes.py
@@ -0,0 +1,41 @@
+"""
+In this lesson, we're going to be looking at movie budget and revenue data. This dataset is perfect for trying out some new tools like scikit-learn to run a linear regression and seaborn, a popular data visualisation library built on top of Matplotlib. 
+
+The question we want to answer today is: Do higher film budgets lead to more revenue in the box office? In other words, should a movie studio spend more on a film to make more? 
+
+
+Today you'll learn:
+
+    How to use a popular data visualisation library called Seaborn
+
+    How to run and interpret a linear regression with scikit-learn
+
+    How to plot a regression a scatter plot to visualise relationships in the data
+
+    How to add a third dimension to a scatter plot to create a bubble chart
+
+    How to cleverly use floor division // to convert your data
+    
+    """
+
+"""
+Learning Points & Summary
+
+Today was a pretty packed lesson where we introduced a lot of new concepts. In this lesson we looked at how to:
+
+    Use nested loops to remove unwanted characters from multiple columns
+
+    Filter Pandas DataFrames based on multiple conditions using both .loc[] and .query()
+
+    Create bubble charts using the Seaborn Library
+
+    Style Seaborn charts using the pre-built styles and by modifying Matplotlib parameters
+
+    Use floor division (i.e., integer division) to convert years to decades
+
+    Use Seaborn to superimpose a linear regressions over our data
+
+    Make a judgement if our regression is good or bad based on how well the model fits our data and the r-squared metric
+
+    Run regressions with scikit-learn and calculate the coefficients. 
+"""
